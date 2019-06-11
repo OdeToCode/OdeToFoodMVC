fdbd58a34d6c6ee70321428573ff1231a0838608
Scott Allen <scott@odetocode.com>
2019-06-11 14:35:50 -0400

    m02-c07

--- OdeToFood/OdeToFood.Web/Controllers/HomeController.cs
+++ OdeToFood/OdeToFood.Web/Controllers/HomeController.cs
@@ -9,7 +9,7 @@ namespace OdeToFood.Web.Controllers
     public class HomeController : Controller
     {
         public ActionResult Index()
-        {
+        {            
             return View();
         }
 
