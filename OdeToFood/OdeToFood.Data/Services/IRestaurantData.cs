02aba8ccfdecaa305b2d659f45f70c607ae2c58e
Scott Allen <scott@odetocode.com>
2019-06-11 11:41:10 -0400

    m02-c06

---
+++ OdeToFood/OdeToFood.Data/Services/IRestaurantData.cs
@@ -0,0 +1,13 @@
+﻿using OdeToFood.Data.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OdeToFood.Data.Services
+{
+    public interface IRestaurantData
+    {
+        IEnumerable<Restaurant> GetAll();
+    }
+}
---
+++ OdeToFood/OdeToFood.Data/Services/InMemoryRestaurantData.cs
@@ -0,0 +1,27 @@
+﻿using OdeToFood.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OdeToFood.Data.Services
+{
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        List<Restaurant> restaurants;
+
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>()
+            {
+                new Restaurant { Id = 1, Name = "Scott's Pizza", Cuisine = CuisineType.Italian},
+                new Restaurant { Id = 2, Name = "Tersiguels", Cuisine = CuisineType.French},
+                new Restaurant { Id = 3, Name = "Mango Grove", Cuisine = CuisineType.Indian},
+            };
+        }
+
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return restaurants.OrderBy(r => r.Name);
+        }
+    }
+}
