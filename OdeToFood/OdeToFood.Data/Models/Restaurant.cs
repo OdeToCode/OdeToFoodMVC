c899d71b08d34a6922256669d216ec7d92fa3edd
Scott Allen <scott@odetocode.com>
2019-06-11 09:46:09 -0400

    m02-c05

---
+++ OdeToFood/OdeToFood.Data/Models/Restaurant.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OdeToFood.Data.Models
+{
+    public class Restaurant
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public CuisineType Cuisine { get; set; }
+    }
+}
