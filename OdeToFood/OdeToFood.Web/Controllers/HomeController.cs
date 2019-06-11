f667ba28acd9f8d17c43697aa55dffba75a51d0f
Scott Allen <scott@odetocode.com>
2019-06-11 14:54:30 -0400

    m02-c08

--- OdeToFood/OdeToFood.Web/Controllers/HomeController.cs
+++ OdeToFood/OdeToFood.Web/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OdeToFood.Data.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +9,17 @@ namespace OdeToFood.Web.Controllers
 {
     public class HomeController : Controller
     {
+        IRestaurantData db;
+
+        public HomeController()
+        {
+            db = new InMemoryRestaurantData();
+        }
+
         public ActionResult Index()
-        {            
-            return View();
+        {
+            var model = db.GetAll();
+            return View(model);
         }
 
         public ActionResult About()
