9d9d826e1425e2f99587ad97a5adb230c2bf2b51
Scott Allen <scott@odetocode.com>
2019-07-08 08:10:01 -0400

    m04-c02

--- OdeToFood/OdeToFood.Web/Controllers/GreetingController.cs
+++ OdeToFood/OdeToFood.Web/Controllers/GreetingController.cs
@@ -11,9 +11,10 @@ namespace OdeToFood.Web.Controllers
     public class GreetingController : Controller
     {
         // GET: Greeting
-        public ActionResult Index()
+        public ActionResult Index(string name)
         {
             var model = new GreetingViewModel();
+            model.Name = name ?? "no name";
             model.Message = ConfigurationManager.AppSettings["message"];
             return View(model);
         }
--- OdeToFood/OdeToFood.Web/Models/GreetingViewModel.cs
+++ OdeToFood/OdeToFood.Web/Models/GreetingViewModel.cs
@@ -8,5 +8,6 @@ namespace OdeToFood.Web.Models
     public class GreetingViewModel
     {
         public string Message { get; set; }
+        public string Name { get; set; }
     }
 }
