37e059df6192af2afb341afd9804207b23a5579e
Scott Allen <scott@odetocode.com>
2019-07-08 11:54:23 -0400

    m04-c03

--- OdeToFood/OdeToFood.Web/Global.asax.cs
+++ OdeToFood/OdeToFood.Web/Global.asax.cs
@@ -5,6 +5,8 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using System.Web.Http;
+
 
 namespace OdeToFood.Web
 {
@@ -14,6 +16,7 @@ namespace OdeToFood.Web
         {
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            GlobalConfiguration.Configure(WebApiConfig.Register);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
             ContainerConfig.RegisterContainer();
