0b27d981f685a06fd3c0136b242ae2e7ba032ef0
Scott Allen <scott@odetocode.com>
2019-07-08 13:26:55 -0400

    m04-c06

--- OdeToFood/OdeToFood.Web/Controllers/RestaurantsController.cs
+++ OdeToFood/OdeToFood.Web/Controllers/RestaurantsController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OdeToFood.Data.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,18 @@ namespace OdeToFood.Web.Controllers
 {
     public class RestaurantsController : Controller
     {
+        private readonly IRestaurantData db;
+
+        public RestaurantsController(IRestaurantData db)
+        {
+            this.db = db;
+        }
+
         // GET: Restaurants
         public ActionResult Index()
         {
-            return View();
+            var model = db.GetAll();
+            return View(model);
         }
     }
 }
