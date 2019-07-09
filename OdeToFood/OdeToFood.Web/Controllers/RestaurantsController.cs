59c61dc372535b2cca9931a2c86509cb5ea97a50
Scott Allen <scott@odetocode.com>
2019-07-09 08:03:26 -0400

    m04-c08:

--- OdeToFood/OdeToFood.Web/Controllers/RestaurantsController.cs
+++ OdeToFood/OdeToFood.Web/Controllers/RestaurantsController.cs
@@ -26,6 +26,10 @@ namespace OdeToFood.Web.Controllers
         public ActionResult Details(int id)
         {
             var model = db.Get(id);
+            if(model == null)
+            {
+                return View("NotFound");
+            }
             return View(model);
         }
     }
