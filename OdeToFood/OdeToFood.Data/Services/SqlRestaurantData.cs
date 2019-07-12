e48235449ff9e769bc929669c3be91a66a812449
Scott Allen <scott@odetocode.com>
2019-07-12 08:28:56 -0400

    m06-c05

---
+++ OdeToFood/OdeToFood.Data/Services/SqlRestaurantData.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OdeToFood.Data.Models;
+
+namespace OdeToFood.Data.Services
+{
+    public class SqlRestaurantData : IRestaurantData
+    {
+        private readonly OdeToFoodDbContext db;
+
+        public SqlRestaurantData(OdeToFoodDbContext db)
+        {
+            this.db = db;
+        }
+
+        public void Add(Restaurant restaurant)
+        {
+            db.Restaurants.Add(restaurant);
+            db.SaveChanges();
+        }
+
+        public Restaurant Get(int id)
+        {
+            return db.Restaurants.FirstOrDefault(r => r.Id == id);
+        }
+
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return from r in db.Restaurants
+                   orderby r.Name
+                   select r;
+        }
+
+        public void Update(Restaurant restaurant)
+        {
+            var entry = db.Entry(restaurant);
+            entry.State = EntityState.Modified;
+            db.SaveChanges();
+        }
+    }
+}
